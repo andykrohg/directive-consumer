0bfb97aea48572930b026e7bd716094602e538d4
Andy Krohg <akrohg@redhat.com>
2019-08-29 12:57:56 -0400

    Add governing mechanism

--- src/main/java/com/redhat/ConsumerRoute.java
+++ src/main/java/com/redhat/ConsumerRoute.java
@@ -1,5 +1,7 @@
 package com.redhat;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.camel.Exchange;
@@ -13,6 +15,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ConsumerRoute extends RouteBuilder {
+	private long time = System.currentTimeMillis();
+	private Map<String, Integer> inputs = new HashMap<String, Integer>() {{
+		put("left", 0);
+		put("right", 0);
+		put("up", 0);
+		put("down", 0);
+	}};
+	
 	@Override
 	public void configure() throws Exception {
 		restConfiguration().component("servlet").bindingMode(RestBindingMode.json);
@@ -28,9 +38,8 @@ public class ConsumerRoute extends RouteBuilder {
 		getContext().addComponent("kafka", kafka);
 		
 		// Consumer for messages which correspond to the "directive" kafka topic
-		from("kafka:directive")
+		from("kafka:directive?synchronous=true")
 			.streamCaching()
-			.log("Message received from Kafka : ${body}")
 			.unmarshal().json(JsonLibrary.Jackson, Map.class)
 			.process(new Processor() {
 				@Override
@@ -38,10 +47,30 @@ public class ConsumerRoute extends RouteBuilder {
 					Map<String, String> body = exchange.getIn().getBody(Map.class);
 					String direction = body.get("direction");
 					String username = body.get("username");
-					log.info("{}: {}", username, direction);
-					Runtime.getRuntime().exec("/Users/akrohg/projects/cliclick/cliclick kd:arrow-" + direction + " w:250 ku:arrow-" + direction);
+					System.out.println(username + ": " + direction);
+										
+					inputs.put(direction, inputs.get(direction) +1);
+					
+					if (System.currentTimeMillis() < time + 25) {
+						return;
+					}
+					
+					//Determine Majority
+					direction = inputs.keySet().parallelStream().max(new Comparator<String>() {
+						@Override
+						public int compare(String o1, String o2) {
+							return inputs.get(o1).compareTo(inputs.get(o2));
+						}
+					}).get();
+										
+					inputs.keySet().parallelStream().forEach(key -> {
+						inputs.put(key, 0);
+					});
+					
+					Process process = Runtime.getRuntime().exec("/Users/akrohg/projects/cliclick/cliclick kd:arrow-" + direction + " w:250 ku:arrow-" + direction);
+					process.waitFor();
+					time = System.currentTimeMillis();
 				}
-			});
-		    
+			});    
 	}
 }
