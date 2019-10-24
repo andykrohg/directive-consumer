36249e993e9ce7c82fa0c2d7bab3e9322cfc950d
Andy Krohg <akrohg@redhat.com>
2019-10-24 07:24:39 -0400

    Change buffer data structure to thread-safe implementation

--- src/main/java/com/redhat/DirectiveProcessor.java
+++ src/main/java/com/redhat/DirectiveProcessor.java
@@ -1,7 +1,8 @@
 package com.redhat;
 
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.stream.Collectors;
 
 import org.apache.camel.Exchange;
@@ -11,17 +12,16 @@ import org.kie.api.runtime.KieSession;
 import org.openqa.selenium.Keys;
 
 public class DirectiveProcessor implements Processor {
-	public static long time = System.currentTimeMillis();
 	public static final String ANSI_RED = "\u001B[31m";
 	public static final String ANSI_WHITE = "\u001B[37m";
 	public static final String ANSI_RESET = "\u001B[0m";
 
 	RemoteCache<String, Integer> userData;
-	List<Map<String, String>> inputs;
+	ArrayBlockingQueue<Map<String, String>> inputs;
 	String color;
 	KieSession kieSession;
 
-	public DirectiveProcessor(RemoteCache<String, Integer> userData, List<Map<String, String>> inputs, String color, KieSession kieSession) {
+	public DirectiveProcessor(RemoteCache<String, Integer> userData, ArrayBlockingQueue<Map<String, String>> inputs, String color, KieSession kieSession) {
 		super();
 		this.kieSession = kieSession;
 		this.userData = userData;
@@ -31,23 +31,14 @@ public class DirectiveProcessor implements Processor {
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		if (ConsumerRoute.gameOver) {
-			return;
-		}
-		Map<String, String> body = exchange.getIn().getBody(Map.class);
-		String direction = body.get("direction");
-		String username = body.get("username");
-
-		String colorTag = color.equals("red") ? ANSI_RED + "[Team Red Hat] " + ANSI_RESET : ANSI_WHITE + "[Team White Hat] " + ANSI_RESET;
-		System.out.println(colorTag + username + ": " + direction);
-
-		inputs.add(body);
-
-		if (System.currentTimeMillis() < time + 25) {
+		if (ConsumerRoute.gameOver || inputs.isEmpty()) {
 			return;
 		}
 
-		final Map<String, Long> totals = inputs.stream()
+		ArrayList<Map<String,String>> buffer = new ArrayList<Map<String, String>>();
+		inputs.drainTo(buffer);
+		
+		final Map<String, Long> totals = buffer.parallelStream()
 				.collect(Collectors.groupingBy(map -> map.get("direction").toString(), Collectors.counting()));
 		
 		final String consensus = determineConsensus(totals);
@@ -85,10 +76,10 @@ public class DirectiveProcessor implements Processor {
 			}
 		}
 
-		ConsumerRoute.bodyElement.sendKeys(new String(new char[650]).replace("\0", key));
+		ConsumerRoute.bodyElement.sendKeys(new String(new char[500]).replace("\0", key));
 				
 		// +1 for consent, -1 for dissent
-		inputs.parallelStream().forEach(input -> {
+		buffer.parallelStream().forEach(input -> {
 			int scoreChange = input.get("direction").equals(consensus) ? 1 : -1;
 			int score = userData.containsKey(input.get("username")) ? userData.get(input.get("username")) + scoreChange
 					: scoreChange;
@@ -96,10 +87,6 @@ public class DirectiveProcessor implements Processor {
 			// push to Data Grid
 			userData.putAsync(input.get("username"), score);
 		});
-
-		// reset the buffer
-		inputs.clear();
-		time = System.currentTimeMillis();
 	}
 
 	public String determineConsensus(Map<String, Long> totals) {
---
+++ src/main/java/com/redhat/InputProcessor.java
@@ -0,0 +1,38 @@
+package com.redhat;
+
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class InputProcessor implements Processor {
+	public static long time = System.currentTimeMillis();
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	public static final String ANSI_RESET = "\u001B[0m";
+
+	ArrayBlockingQueue<Map<String, String>> inputs;
+	String color;
+
+	public InputProcessor(ArrayBlockingQueue<Map<String, String>> inputs, String color) {
+		super();
+		this.inputs = inputs;
+		this.color = color;
+	}
+	
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		if (ConsumerRoute.gameOver) {
+			return;
+		}
+		Map<String, String> body = exchange.getIn().getBody(Map.class);
+		String direction = body.get("direction");
+		String username = body.get("username");
+
+		String colorTag = color.equals("red") ? ANSI_RED + "[Team Red Hat] " + ANSI_RESET : ANSI_WHITE + "[Team White Hat] " + ANSI_RESET;
+		System.out.println(colorTag + username + ": " + direction);
+
+		inputs.add(body);
+	}
+}
